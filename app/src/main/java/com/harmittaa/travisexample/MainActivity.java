3c9feb76d4abe2d8a798fd2bde2d752c88bab393
Dima <k7sdima@gmail.com>
2018-06-10 19:12:19 +0300

    travis start

--- app/src/main/java/com/harmittaa/travisexample/MainActivity.java
+++ app/src/main/java/com/harmittaa/travisexample/MainActivity.java
@@ -1,15 +1,12 @@
 package com.harmittaa.travisexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 }
